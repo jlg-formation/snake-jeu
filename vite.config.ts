4985c6af30671d1a130027104f77850a4ac1d491
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-07-11 14:22:24 +0100

    chore: add pnpm workspace configuration for built dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ command }) => {
+  
+  const base = command === "build" ? "/snake-jeu/" : "/";
+  
+  return {
+    base,
+    plugins: [react(), tailwindcss()],
+  };
+});
